5e5e4a1cdee8ecbbb24611a18e2e72fced7dafac
Althaf <althafrahman980@gmail.com>
2023-03-11 14:32:08 +0530

    Project name updated

--- petstories/wsgi.py
+++ petstories/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for social_book project.
+WSGI config for petstories project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_book.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'petstories.settings')
 
 application = get_wsgi_application()
